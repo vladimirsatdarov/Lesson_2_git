544aba2756dbb756ff54d7d1baa4f3cbb096597c
vladimirsatdarov <150793663+vladimirsatdarov@users.noreply.github.com>
2025-07-09 14:53:35 +0200

    Update Main.java

--- src/main/java/de/training_git/Main.java
+++ src/main/java/de/training_git/Main.java
@@ -5,12 +5,10 @@ package de.training_git;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("GOODBYE qa.guru!\n");
+        System.out.printf("qa.guru kruto!\n");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int j = 1; j <= 5; j++) {
+            System.out.println("j = " + i);
         }
     }
 }
